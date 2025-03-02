36fc7cc15bcba568a66387c3217d2d674f82ed7f
55garima <myselfgarimasingh555@gmail.com>
2025-03-02 22:59:39 +0530

    bio add and db migration

--- app/models/user.py
+++ app/models/user.py
@@ -17,11 +17,12 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     is_active = db.Column(db.Boolean, default=True)
+    bio = db.Column(db.String(256), nullable=True)
     
     
     def __init__(self, username, email, password, first_name=None, last_name=None):
         """Initialize a new user.
-        
+
         
         Args:
             username (str): The username
