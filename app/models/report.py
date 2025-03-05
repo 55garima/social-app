2a7902bd4b1e95381d31f33c14e8dec91177e380
55garima <myselfgarimasingh555@gmail.com>
2025-03-05 19:58:22 +0530

    added report model

---
+++ app/models/report.py
@@ -0,0 +1,16 @@
+"""Report model for the application."""
+from config.database import db 
+
+class Report(db.Model):
+    """Report Model for banned user"""
+    
+    __tablename__ = 'reports'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    reason = db.Column(db.String(256), nullable=False)
+
+    def __init__(self, user_id, reason):
+        self.user_id = user_id
+        self.reason = reason
+   
